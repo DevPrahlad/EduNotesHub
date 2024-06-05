33f3cfd6a1c27774e54a866a3240e096e66400d8
Prahlad Singh <145636965+DevPrahlad@users.noreply.github.com>
2024-06-05 20:14:30 +0530

    Update userlogin.php

--- userarea/userlogin.php
+++ userarea/userlogin.php
@@ -1,15 +1,19 @@
 <?php
-
+session_start();
 error_reporting(0);
 include('../config.php');
 
-// Check if user is already logged in, if yes, redirect to main page
-if(isset($_SESSION['email'])) {
-    header('Location: ../main.php');
+// Check if user is already logged in, if yes, redirect to the redirect URL or main page
+if (isset($_SESSION['email'])) {
+    if (isset($_GET['redirect']) && !empty($_GET['redirect'])) {
+        header('Location: ' . $_GET['redirect']);
+    } else {
+        header('Location: ../main.php');
+    }
     exit; // Stop further execution
 }
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $email = mysqli_real_escape_string($con, $_POST['email']);
     $password = mysqli_real_escape_string($con, $_POST['password']);
 
@@ -19,13 +23,15 @@ if(isset($_POST['submit'])){
     mysqli_stmt_store_result($query);
     $count = mysqli_stmt_num_rows($query);
 
-    if($count > 0){
+    if ($count > 0) {
         mysqli_stmt_bind_result($query, $id, $email, $password); // Update with actual column names
         mysqli_stmt_fetch($query);
         $_SESSION['email'] = $email;
 
+        $redirect = isset($_POST['redirect']) && !empty($_POST['redirect']) ? $_POST['redirect'] : '../main.php';
+        
         if ($_SESSION['email'] == 'prahladsingh7512@gmail.com') {
-            header('location:../main.php');
+            header('Location: ' . $redirect);
             exit; // Stop further execution
         } else {
             $adminemail = "prahlad.singh.education@gmail.com";
@@ -33,14 +39,13 @@ if(isset($_POST['submit'])){
             $message = "A user has logged in.\nEmail: $email";
 
             mail($adminemail, $subject, $message);
-            header('location:../main.php');
+            header('Location: ' . $redirect);
             exit; // Stop further execution
         }
     } else {
         $message[] = 'Incorrect email or password!';
     }
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,12 +54,10 @@ if(isset($_POST['submit'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2268170979958753"
      crossorigin="anonymous"></script>
-   <linl rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-      <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
-   
-   <linl rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login | EduNotesHub</title>
+   <title>Login | EduNotesHub</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
@@ -63,7 +66,7 @@ h5 {
   text-align: right;
 }
 
-input[type="checkbox"] {   }
+input[type="checkbox"] { }
 
 </style>
 </head>
@@ -74,16 +77,17 @@ input[type="checkbox"] {   }
    <form action="" method="post" enctype="multipart/form-data">
       <h3>login now</h3>
       <?php
-      if(isset($message)){
-         foreach($message as $message){
+      if (isset($message)) {
+         foreach ($message as $message) {
             echo '<div class="message">'.$message.'</div>';
          }
       }
       ?>
+      <input type="hidden" name="redirect" value="<?php echo isset($_GET['redirect']) ? htmlspecialchars($_GET['redirect']) : ''; ?>">
       <input type="email" name="email" placeholder="enter email" class="box" required>
-<h5> <input type="checkbox" onclick="myFunction()">&nbsp;Show Password</h5>
+      <h5><input type="checkbox" onclick="myFunction()">&nbsp;Show Password</h5>
       <input id="myInput" type="password" name="password" placeholder="enter password" class="box" required>
-     <h5>Forgot <a href="forgot_password.php">password?</a></h5>
+      <h5>Forgot <a href="forgot_password.php">password?</a></h5>
       <input type="submit" name="submit" value="login now" class="btn">
       <p>don't have an account? <a href="registration.php">register now</a></p>
    </form>
